0314ea52315bba050ef0fc2d7b3bc303f3fb22c2
sarrru <sharusura123@gmail.com>
2024-12-09 21:37:10 +0545

    splash_screen

---
+++ lib/view/splash_screen.dart
@@ -0,0 +1,58 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    // Set a timer to navigate after 4 seconds
+    Timer(const Duration(seconds: 4), () {
+      Navigator.pushReplacement(
+        context,
+        MaterialPageRoute(builder: (context) => const HomeScreen()),
+      );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor:
+          const Color.fromARGB(255, 195, 141, 205), // Adjust background color
+      body: Center(
+        child: Image.asset(
+          'assets/images/splash.png', // Replace with your image path
+          width: 400, // Adjust size as needed
+          height: 400,
+        ),
+      ),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Home Screen'),
+      ),
+      body: const Center(
+        child: Text(
+          'Welcome to Home Screen!',
+          style: TextStyle(fontSize: 20),
+        ),
+      ),
+    );
+  }
+}
