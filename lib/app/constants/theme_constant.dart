a7b167daf95e2fa35670f523c68e5682bc74f83e
sarrru <123716436+sarrru@users.noreply.github.com>
2025-01-31 16:13:43 +0545

    some change

--- lib/app/constants/theme_constant.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class ThemeConstant {
-  ThemeConstant._();
-  static final Color darkPrimaryColor = Colors.yellow.shade800;
-  static final Color primaryColor =
-      const Color.fromARGB(255, 142, 15, 57); // Use pink[700]
-  static final Color appBarColor = Colors.pink.shade800; // Use pink[700]
-}
